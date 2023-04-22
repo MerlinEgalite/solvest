6956d3fb876bd1dede810c72c42573e767e5cffe
MerlinEgalite <egalite.merlin@gmail.com>
2023-04-22 15:53:34 +0200

    fix: no strict inequalities

--- test/MintVest.t.sol
+++ test/MintVest.t.sol
@@ -41,7 +41,7 @@ contract MintVestTest is BaseTest {
         uint256 claimTime
     ) public {
         receiver = _boundAddressNotZero(receiver);
-        start = bound(start, OFFSET, block.timestamp + TWENTY_YEARS);
+        start = _boundStart(start);
         duration = bound(duration, OFFSET, TWENTY_YEARS);
         cliff = bound(cliff, 0, duration);
         claimTime = bound(claimTime, start + cliff, type(uint128).max);
