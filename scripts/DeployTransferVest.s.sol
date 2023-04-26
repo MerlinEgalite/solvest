e9ca58d3d8636999d811e6557cf752512db5fbdf
PraneshASP <praneshas2000@gmail.com>
2023-04-26 22:33:21 +0700

    feat: add deployment script for TransferVest

---
+++ scripts/DeployTransferVest.s.sol
@@ -0,0 +1,21 @@
+pragma solidity 0.8.17;
+
+import "forge-std/Script.sol";
+
+import {TransferVest} from "../src/TransferVest.sol";
+
+contract DeployTransferVest is Script{
+
+    /// @dev Change these values before running the script!
+    address public owner = address(0xCAFE);
+    address public token = address(0xBEEF); 
+    address public sender = address(0xFACE); 
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        address deployedInstance = address(new TransferVest(owner, sender, token));
+        console.log("TransferVest contract deployed at: ",deployedInstance, " for the token: ", token);
+        vm.stopBroadcast();
+    }
+}
